0c8368e87000476be0e28b638d9ce5e99940b751
gilrnet <gilrnet@gmail.com>
2020-09-23 19:13:06 -0300

    Inserindo Apostas

---
+++ ProtocolosPOOHerancaPolimorfismo/Apostas/Apostas/Notificacao.swift
@@ -0,0 +1,13 @@
+//
+//  Notificacao.swift
+//  Apostas
+//
+//  Created by Gilvã Rocha on 19/09/20.
+//  Copyright © 2020 Gilvã Rocha. All rights reserved.
+//
+
+import Foundation
+
+protocol Notificacao {
+    func notificar()
+}
